3a0ae44b4131ff1fc69fa95ff3f0f7eb8873ed76
Pipo Clown <spiraltrain@gmail.com>
2025-06-23 13:32:40 +0200

    Update MavenDemoApplication.java
    test comment

--- src/main/java/com/jetbrains/mavendemo/MavenDemoApplication.java
+++ src/main/java/com/jetbrains/mavendemo/MavenDemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MavenDemoApplication {
+	// test commit
 	public static void main(String[] args) {
 		SpringApplication.run(MavenDemoApplication.class, args);
 	}
